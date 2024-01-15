d65a6cdee80dbbb8e663a1af1ce93222714363d8
ming yang shih <39481555+mingyangshih@users.noreply.github.com>
2024-01-15 17:35:37 +0800

    use background js add content js

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,28 @@
-/* chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.includes('netflix')) {
-    chrome.tabs.remove(tabId);
-  }
-}); */
+chrome.runtime.onInstalled.addListener(() => {
+  let extensionActive = false
+  setBadgeText()
+  const openConsoleScript = {
+    id: "openConsoleScript",
+    js: ["scripts/content.js"],
+    world: "MAIN",
+    matches: ["https://*/*"]
+  };
+  chrome.action.onClicked.addListener(async (tab) => {
+    let tabId = tab.id
+    extensionActive = !extensionActive
+    setBadgeText()
+    if(extensionActive) {
+      chrome.scripting.executeScript({
+      target : {tabId : tabId},
+      world: "MAIN",
+      files : [ "scripts/content.js" ],
+    })
+    .then(() => console.log("injected script file"));
+    }
+  });
+  function setBadgeText() {
+    chrome.action.setBadgeText({
+      text: extensionActive? "ON":"OFF",
+    });
+  };
+});
