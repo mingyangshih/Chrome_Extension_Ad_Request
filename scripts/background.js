2eb312db7629ad6c5c29b13923e535f51f13ebbc
ming yang shih <39481555+mingyangshih@users.noreply.github.com>
2024-01-18 17:16:16 +0800

    fix different tab issue

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
   let extensionActive = false;
+  let extensionActiveUrl = {};
   // Tabs Query Options
   let queryOptions = { currentWindow: true, active: true };
   let openConsoleActiveUrl;
-
-  //setBadgeText();
 
   // let openConsoleScript = {
   //   id: "openConsoleScript",
@@ -16,14 +15,17 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.onClicked.addListener(async (tab) => {
     let tabId = tab.id;
     extensionActive = !extensionActive;
+
+    extensionActiveUrl[tabId] = !extensionActiveUrl[tabId] ? true : false;
     setBadgeText(tabId);
-    // Get the current page's url.
+    // Chrome query API
     chrome.tabs.query(queryOptions, (tab) => {
-      openConsoleActiveUrl = tab[0]['url'];
+      // Get the current page's url.
+      openConsoleActiveUrl = tab[0]["url"];
       // Run Before the page refresh.
       chrome.webNavigation.onBeforeNavigate.addListener(
         () => {
-          extensionActive = false;
+          extensionActiveUrl[tabId] = false;
           setBadgeText(tabId);
         },
         { url: [{ urlEquals: openConsoleActiveUrl }] }
@@ -33,10 +35,10 @@ chrome.runtime.onInstalled.addListener(() => {
         chrome.scripting
           .executeScript({
             target: { tabId: tabId },
-            world: 'MAIN',
-            files: ['scripts/content_openConsole.js'],
+            world: "MAIN",
+            files: ["scripts/content_openConsole.js"],
           })
-          .then(() => console.log('injected script file'));
+          .then(() => console.log("injected script file"));
         // Use registerContentScripts
         // chrome.scripting.registerContentScripts([openConsoleScript], () => {
 
@@ -45,17 +47,17 @@ chrome.runtime.onInstalled.addListener(() => {
         chrome.scripting
           .executeScript({
             target: { tabId: tabId },
-            world: 'MAIN',
-            files: ['scripts/content_closeConsole.js'],
+            world: "MAIN",
+            files: ["scripts/content_closeConsole.js"],
           })
-          .then(() => console.log('injected script file'));
+          .then(() => console.log("injected script file"));
       }
     });
   });
   function setBadgeText(tabId) {
     chrome.action.setBadgeText({
       tabId,
-      text: extensionActive ? 'ON' : 'OFF',
+      text: extensionActiveUrl[tabId] ? "ON" : "OFF",
     });
   }
 });
